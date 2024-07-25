a3f11a0e973f2f3a7d8990400c698bd23a66d1e9
Nishant Baser <nishant.baser@intelliatech.com>
2024-07-25 11:04:10 +0530

    added route file

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <div className='bg-customOrange flex justify-center items-center w-full h-[100vh]  font-bold text-5xl text-red'>Route Not Found</div>
+    <div className=' flex justify-center items-center w-full h-[100vh]  font-bold text-xl text-red'>Route Not Found</div>
   )
 }
 
